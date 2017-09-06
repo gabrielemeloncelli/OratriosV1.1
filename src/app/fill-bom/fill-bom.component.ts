88c482dfca08be4bc880fdba4683a9f054f154a9
Gabriele Meloncelli <gabriele.meloncelli@2tinfoservice.it>
2017-09-06 17:13:24 +0200

    Toggle tree management

--- src/app/fill-bom/fill-bom.component.ts
+++ src/app/fill-bom/fill-bom.component.ts
@@ -62,7 +62,6 @@ export class FillBomComponent implements BubbleNodeMessageInterface, OnInit {
     private selectorService: NodeSelectorService) {
     this.treeNodeService = treeNodeService;
     this.coreEstService = coreEstService;
-    this.uiStatusService.treeToolbarVisible = true; // TODO: remove
   }
 
   @ViewChild('navigateModal')
