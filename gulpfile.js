804e585cf9b23b8c2b429777ae7ef51089b4c4fe
Gabriele Meloncelli <gabriele.b.meloncelli@gmail.com>
2018-09-24 15:18:30 +0200

    Update the publish path and files

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
 var gulp = require('gulp');
 
+var basePaths = {
+  dest: 'C:/Publish/Oratrios'
+};
 gulp.task('default', function() {
   // javascript
   gulp.src('dist/*.js')
-  .pipe(gulp.dest('C:/KendoTestPublish'));
+  .pipe(gulp.dest(basePaths.dest));
   // favicon
   gulp.src('dist/*.ico')
-  .pipe(gulp.dest('C:/KendoTestPublish'));
+  .pipe(gulp.dest(basePaths.dest));
   // glyphicons
   gulp.src('dist/*.woff2')
-  .pipe(gulp.dest('C:/KendoTestPublish'));
+  .pipe(gulp.dest(basePaths.dest));
   gulp.src('dist/*.svg')
-  .pipe(gulp.dest('C:/KendoTestPublish'));
+  .pipe(gulp.dest(basePaths.dest));
   gulp.src('dist/*.ttf')
-  .pipe(gulp.dest('C:/KendoTestPublish'));
+  .pipe(gulp.dest(basePaths.dest));
   gulp.src('dist/*.eot')
-  .pipe(gulp.dest('C:/KendoTestPublish'));
+  .pipe(gulp.dest(basePaths.dest));
   gulp.src('dist/*.woff')
-  .pipe(gulp.dest('C:/KendoTestPublish'));
+  .pipe(gulp.dest(basePaths.dest));
   // assets
   gulp.src('dist/assets/**/*.*')
-  .pipe(gulp.dest('C:/KendoTestPublish/assets'));
+  .pipe(gulp.dest(basePaths.dest));
 
 });
