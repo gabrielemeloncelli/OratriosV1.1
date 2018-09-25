74c12c259814360f4c2028aad2cfb40940227c18
Gabriele Meloncelli <gabriele.b.meloncelli@gmail.com>
2018-09-25 16:11:56 +0200

    Add set-refspec pagination

--- src/app/set-refspec/set-refspec.component.ts
+++ src/app/set-refspec/set-refspec.component.ts
@@ -6,6 +6,12 @@ import {
 import { RefSpec } from '../core/ref-spec';
 import { RefSpecDto } from '../core/ref-spec-dto';
 
+enum RefSpecFilterType {
+  None = 1, 
+  Commodity,
+  Tag
+}
+
 @Component({
     templateUrl: 'set-refspec.component.html',
     styleUrls: ['set-refspec.component.css'],
@@ -16,8 +22,14 @@ import { RefSpecDto } from '../core/ref-spec-dto';
     public tagTemplate: string;
     public refspecs: RefSpec[];
     public isBusy: boolean;
+    public totItems: number;
+    public currentFilter: RefSpecFilterType;
+    private _generatedDtos: RefSpecDto[];
+
     ngOnInit() {
       this.refspecs = [];
+      this.currentFilter = RefSpecFilterType.None;
+      this._generatedDtos = this.generateDtos();
     }
     public setDirty(idx: number): void {
       this.refspecs[idx].dirty = true;
@@ -26,28 +38,50 @@ import { RefSpecDto } from '../core/ref-spec-dto';
     public saveDirty(idx: number): void {
       this.refspecs[idx].dirty = false;
     }
-    public findCommodity(): void {
+    public findCommodity(skip = 0, take = 10): void {
       this.setBusy();
+      this.currentFilter = RefSpecFilterType.Commodity;
       setTimeout(() => {
-        this.completeCommodity();        
+        this.completeCommodity(skip, take);        
       }, 500);
     }
 
-    public findTag(): void {
+    public findTag(skip = 0, take = 10): void {
       this.setBusy();
+      this.currentFilter = RefSpecFilterType.Tag;
       setTimeout(() => {
-        this.completeTag();        
+        this.completeTag(skip, take);        
       }, 500);
 
     }
 
-    public findAll(): void {
+    public findAll(skip = 0, take = 10): void {
       this.setBusy();
+      this.currentFilter = RefSpecFilterType.None;
       setTimeout(() => {
-        this.completeAll();        
+        this.completeAll(skip, take);        
       }, 500);
     }
 
+    private pageChanged(pageNumber: number): void {
+      switch(this.currentFilter){
+        case RefSpecFilterType.None: {
+          this.findAll((pageNumber - 1) * 10, 10);
+          break;
+        }
+        case RefSpecFilterType.Commodity: {
+          this.findCommodity((pageNumber - 1) * 10, 10);
+          break;
+        }
+        case RefSpecFilterType.Tag: {
+          this.findTag((pageNumber - 1) * 10, 10);
+          break;
+        }
+      }
+
+    }
+
+    
     private generateDtos(): RefSpecDto[] {
       var dtos: RefSpecDto[] = [];
       var dto = new RefSpecDto();
@@ -61,53 +95,83 @@ import { RefSpecDto } from '../core/ref-spec-dto';
       dto.tag = "TAG00001";
       dto.refSpec = "RefSpec002";
       dtos.push(dto);
-      dto = new RefSpecDto();
-      dto.materialId = 0;
-      dto.tag = "TAG00002";
-      dto.refSpec = "RefSpec003";
-      dtos.push(dto);
+      var i: number;
+      for(i = 0; i < 23; i++) {
+        dto = new RefSpecDto();
+        dto.materialId = 100 + i * 12;
+        dto.commodityCode = "COMMODITY".concat((i + 10).toString());
+        dto.refSpec = "RefSpec0".concat((i + 13).toString());
+        dtos.push(dto);
+      }
+      for(i = 0; i < 12; i++) {
+        dto = new RefSpecDto();
+        dto.materialId = 0;
+        dto.tag = "TAG000".concat((i + 27).toString());
+        dto.refSpec = "RefSpec0".concat((i + 44).toString());
+        dtos.push(dto);  
+      }      
       return dtos;
     }
 
     private setBusy(): void {
-      this.isBusy = true;
+      setTimeout(() => {this.isBusy = true}, 100);
     }
 
     private resetBusy(): void {
-      this.isBusy = false;
+      setTimeout(() => {this.isBusy = false}, 100);
     }
 
-    private completeCommodity(): void {
+    private completeCommodity(skip: number, take: number): void {
+      var counter = 0;
       this.refspecs = [];
-      for(let dto of this.generateDtos())
+      for(let dto of this._generatedDtos)
       {
         if(dto.materialId > 0 && dto.commodityCode.indexOf(this.commodityTemplate) >= 0) {
-          this.refspecs.push(new RefSpec(dto));
+          if (counter >= skip && counter < skip + take) {
+            this.refspecs.push(new RefSpec(dto));
+          }
+          counter++;
         }
       }
+      this.totItems = counter;
+      console.log("totItems: " + this.totItems);
       this.resetBusy();
     }
 
-    private completeTag(): void {
+    private completeTag(skip: number, take: number): void {
       this.refspecs = [];
-      for(let dto of this.generateDtos())
-      {
+      var counter = 0;
+      for(let dto of this._generatedDtos)
+      {        
         if(dto.materialId == 0 && dto.tag.indexOf(this.tagTemplate) >= 0) {
-          this.refspecs.push(new RefSpec(dto));
+          if (counter >= skip && counter < skip + take) {
+            this.refspecs.push(new RefSpec(dto));
+          }
+          counter++;
         }
       }
+      this.totItems = counter;
+      console.log("totItems: " + this.totItems);
       this.resetBusy();
     }
 
-    private completeAll(): void {      
+    private completeAll(skip: number, take: number): void {      
       this.refspecs = [];
-      for(let dto of this.generateDtos())
+      var counter = 0;
+      for(let dto of this._generatedDtos)
       {
-        this.refspecs.push(new RefSpec(dto));
+        if (counter >= skip && counter < skip + take) {
+          this.refspecs.push(new RefSpec(dto));
+        }
+        counter++;
       }
+      this.totItems = counter;
+      console.log("totItems: " + this.totItems);
       this.resetBusy();
 
     }
+
+
 
 
 
--- src/app/set-refspec/set-refspec.module.ts
+++ src/app/set-refspec/set-refspec.module.ts
@@ -7,13 +7,15 @@ import { SetRefspecComponent } from './set-refspec.component';
 import { SetRefspecRoutingModule } from './set-refspec-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { fromPromise } from 'rxjs/observable/fromPromise';
+import { PagerModule } from '../pager/pager.module';
 
 @NgModule({
     imports: [
     CommonModule,
     SetRefspecRoutingModule,
     SharedModule,
-    FormsModule
+    FormsModule,
+    PagerModule
   ],
   declarations: [ SetRefspecComponent ]
 })
